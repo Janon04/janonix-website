8c32e97509af8cd0f47668325ed6dbea900c29aa
janon04 <janon3030@gmail.com>
2025-11-07 23:56:53 +0200

    Add favicon (SVG), link in base template and redirect for /favicon.ico

--- janonix_website/urls.py
+++ janonix_website/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Redirect legacy /favicon.ico requests to the static favicon SVG
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.svg')),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
